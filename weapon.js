8e7a4988484ba7f9cdb035c3acb40a665f296299
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2022-03-23 10:33:07 -0400

    Add files via upload

--- weapon.js
+++ weapon.js
@@ -2,12 +2,12 @@ export class Weapon {
     constructor (name){
         this.name = name;
         if (this.name == "smg"){
-            this.cooldown = 20;
+            this.cooldown = 100;
             this.lastFired = 0;
             this.damage = 20;
             this.speed = 20;
         } else if (this.name == "pistol"){
-            this.cooldown = 120;
+            this.cooldown = 700;
             this.lastFired = 0;
             this.damage = 10;
             this.speed = 10;
